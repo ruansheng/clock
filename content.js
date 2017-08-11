73a6f6980aa3d0f2008e599257c4d95bcabdb4aa
ruan.sheng <ruan.sheng@immomo.com>
2017-08-11 15:46:08 +0800

    init

---
+++ content.js
@@ -0,0 +1,8 @@
+﻿// 接收popup.js的消息
+chrome.extension.onMessage.addListener(
+  	function(request, sender, sendResponse) {
+    	if(request.type == "popup_get_url") {
+			var url = window.location.href;
+			sendResponse({url:url}); 
+		}
+});
---
+++ popup.js
@@ -0,0 +1,22 @@
+﻿$(function() {
+	
+	function getDate(timestamp){
+		var d = new Date(timestamp * 1000);
+		var date = (d.getFullYear()) + "-" + (d.getMonth() + 1) + "-" + (d.getDate()) + " " + (d.getHours()) + ":" + (d.getMinutes()) + ":" + (d.getSeconds());
+		return date;
+	}
+	
+	// 当前时间戳
+	var now_timestamp = parseInt(new Date().getTime()/1000);
+	$('#now_timestamp').val(now_timestamp);
+	
+	// 当前时间字符串
+	var now_datestring = getDate(now_timestamp);
+	$('#now_datestring').val(now_datestring);
+	
+	$('#button_time').click(function() {
+		var time = $('#input_time').val();
+		var date = getDate(time);
+		$('#output_time').val(date);		
+	});
+})
